a9559c49b321fc098c3883740b7d8015b77b2e8d
andersseen <andriipap01@gmail.com>
2025-07-08 20:42:27 +0200

    Add ContactSection

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
       },
       {
         path: 'contact',
-        loadComponent: () => import('@/components/contact'),
+        loadComponent: () => import('@/components/form/contact-section'),
       },
     ],
   },
